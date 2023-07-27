b801ea50b86d5b7e3978e49b5dfb0fc0694973f1
Ken Leung <kenleung5e28@gmail.com>
2023-07-27 17:00:58 +0800

    try CancellationToken and JoinSet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+use tokio::select;
+use tokio::task::JoinSet;
+use tokio::time::{sleep, Duration};
+use tokio_util::sync::CancellationToken;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let cancel_token = CancellationToken::new();
+    let mut set = JoinSet::new();
+    for i in 0..10 {
+        let cancel_token_clone = cancel_token.clone();
+        set.spawn(async move {
+            select! {
+                _ = sleep(Duration::from_secs(10 - i)) => {
+                    println!("Thread {} awakes", i);
+                }
+                _ = cancel_token_clone.cancelled() => {
+                    println!("Thread {} receives cancellation signal", i)
+                }
+            }
+            i
+        });
+    }
+    sleep(Duration::from_secs(3)).await;
+    cancel_token.cancel();
+    while let Some(res) = set.join_next().await {
+        match res {
+            Ok(i) => println!("Thread {} completes", i),
+            Err(e) => println!("Error occurs: {}", e.to_string()),
+        }
+    }
 }
